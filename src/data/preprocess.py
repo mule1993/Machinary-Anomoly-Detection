13edc66f168ba5c344a857e03c56ad7170cf9d4f
mule1993 <muluneh.gashaw@gmail.com>
2026-05-12 16:06:23 +0300

    MLflow, S3, data processing

---
+++ src/data/preprocess.py
@@ -0,0 +1,108 @@
+import pandas as pd
+from ingest import load_csv_from_s3, upload_csv_to_s3
+
+# Load data from S3
+df = load_csv_from_s3()
+
+
+def kelvin_to_celsius(df):
+    """
+    Convert air and process temperature columns from Kelvin to Celsius.
+
+    Args:
+        df: DataFrame with temperature columns
+
+    Returns:
+        DataFrame with converted temperature values
+    """
+    if "Air temperature [K]" in df.columns:
+        df["Air temperature [K]"] = df["Air temperature [K]"] - 273.15
+        df.rename(columns={"Air temperature [K]": "Air temperature [C]"}, inplace=True)
+    if "Process temperature [K]" in df.columns:
+        df["Process temperature [K]"] = df["Process temperature [K]"] - 273.15
+        df.rename(
+            columns={"Process temperature [K]": "Process temperature [C]"}, inplace=True
+        )
+    return df
+
+
+def drop_rows_with_negatives(df):
+    """
+    Drop rows that contain negative numbers in numerical columns.
+
+    Args:
+        df: DataFrame
+
+    Returns:
+        DataFrame with rows containing negatives dropped
+    """
+    numerical_cols = df.select_dtypes(include="number").columns
+    mask = (df[numerical_cols] < 0).any(axis=1)
+    return df[~mask]
+
+
+def speed_torque_to_power(df):
+    """
+    Convert Rotational speed [rpm]	Torque [Nm] to Power [KW]
+    Args:
+        df: DataFrame with Rotational speed [rpm]	Torque [Nm] columns
+
+    Returns:
+        DataFrame with converted Power values
+    """
+    if "Rotational speed [rpm]" in df.columns and "Torque [Nm]" in df.columns:
+        df.insert(
+            5, "Power [KW]", df["Rotational speed [rpm]"] * df["Torque [Nm]"] / 9550
+        )
+        df.drop(columns=["Rotational speed [rpm]", "Torque [Nm]"], inplace=True)
+    return df
+
+
+def drop_unwanted_columns(df):
+    """
+    Drop columns that are not needed for modeling.
+
+    Args:
+        df: DataFrame with columns to drop ('Product ID', 'Failure Type')
+
+    Returns:
+        DataFrame with unwanted columns dropped
+    """
+    columns_to_drop = ["Product ID", "TWF", "HDF", "PWF", "OSF", "RNF"]
+    return df.drop(columns=columns_to_drop, errors="ignore")
+
+
+def one_hot_encode_types(df):
+    """
+    One-hot encode the 'Type' column.
+
+    Args:
+        df: DataFrame with 'Type' column
+    Returns:
+        DataFrame with one-hot encoded 'Type' column
+    """
+    if "Type" in df.columns:
+        # 1. Get the integer position of the 'Type' column
+        idx = df.columns.get_loc("Type")
+        # 2. Create the dummies
+        type_dummies = pd.get_dummies(df["Type"], prefix="Type", dtype=int)
+        # 3. Slice the dataframe and sandwich the dummies in the middle
+        df = pd.concat([df.iloc[:, :idx], type_dummies, df.iloc[:, idx + 1 :]], axis=1)
+    return df
+
+
+if __name__ == "__main__":
+    print(df.head())
+    celsius_df = kelvin_to_celsius(df)
+    print(celsius_df.head())
+    # print("\nAfter dropping rows with negative values:\n")
+    positive_df = drop_rows_with_negatives(celsius_df)
+    print(positive_df.head())
+    power_df = speed_torque_to_power(positive_df)
+    print(power_df.head())
+    final_df = drop_unwanted_columns(power_df)
+    print(final_df.head())
+    encoded_df = one_hot_encode_types(final_df)
+    print(encoded_df.head())
+    print(f"Final DataFrame shape: {encoded_df.shape}")
+    upload_csv_to_s3(encoded_df)
--- src/models/train.py
+++ src/models/train.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+from sys import meta_path
 
 import joblib
-from sklearn.metrics import classification_report
+import mlflow
+from sklearn.metrics import classification_report, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 
 from src.data.ingest import load_csv_from_s3
@@ -29,13 +31,46 @@ class DummyFailureModel:
         return [0] * len(X)
 
 
-# Save the model
-model = DummyFailureModel()
-model_path = MODEL_DIR / "DummyFailureModel.joblib"
-joblib.dump(model, model_path)
+# Configure MLflow to use a local database
+# mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+# AWS: Amazon RDS (Relational Database Service) running PostgreSQL or MySQL.
+# Pmlflow.set_tracking_uri("http://internal-mlflow-load-balancer.aws.com:5000")
+mlflow.set_experiment("Second_Dummy_Model_Experiment")
+
+# Save the model and log it with MLflow
+with mlflow.start_run():
+    mlflow.set_tag("model_type", "dummy")
+    mlflow.log_param("data_source", meta_path)
+
+    model = DummyFailureModel()
+    model_path = MODEL_DIR / "DummyFailureModel.joblib"
+    # Save the model artifact to S3 and log it with MLflow
+    mlflow.log_artifact(str(model_path))
+    # Save the model package using MLflow's sklearn flavor on S3
+    mlflow.sklearn.log_model(
+        sk_model=model, name="model_output", serialization_format="pickle"
+    )
+
+    # Save the model locally
+    joblib.dump(model, model_path)
+
 
 # Predict on the test set
 y_pred = model.predict(X_test)
 
+
+# Log evaluation metrics to MLflow
+with mlflow.start_run(run_name="Evaluation_Step", nested=True):
+    precision = precision_score(y_test, y_pred, average="macro", zero_division=0)
+    recall = recall_score(y_test, y_pred, average="macro", zero_division=0)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+
 # Evaluate the model
 print(classification_report(y_test, y_pred, zero_division=0))
+print(
+    f"Precision: {
+        precision_score(y_test, y_pred, average='macro', zero_division=0):.4f}"
+)
+print(f"Recall: {recall_score(y_test, y_pred, average='macro', zero_division=0):.4f}")
