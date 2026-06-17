a12e773df3478f61efc4f0600193fa2def589605
mule1993 <muluneh.gashaw@gmail.com>
2026-06-17 15:21:39 +0300

    add retraining

--- src/models/retraining.py
+++ src/models/retraining.py
@@ -21,10 +21,6 @@ dynamic_scale_weight = 1
 # ==============================================================
 
 
-<<<<<<< HEAD
-# @hydra.main(version_base=None, config_path="../../config/", config_name="config")
-=======
->>>>>>> 53adc2763cd7db7a5134fbe9d366b665e3c60972
 @hydra.main(version_base=None, config_path="../../config", config_name="config")
 def retraining(config: DictConfig):
     OmegaConf.set_readonly(config, False)
