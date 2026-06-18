4ee0eef24d28486a0bdd509721da3945691d18de
mule1993 <muluneh.gashaw@gmail.com>
2026-06-18 10:36:13 +0300

    retrain fix

--- src/models/retraining.py
+++ src/models/retraining.py
@@ -35,13 +35,7 @@ dynamic_scale_weight = 1
 # ==============================================================
 
 
-# @hydra.main(version_base=None, config_path="../../config", config_name="config")
-# def retraining(config: DictConfig):
-#    OmegaConf.set_readonly(config, False)
-config_directory = "/config" if os.path.exists("/config") else "../../config"
-
-
-@hydra.main(version_base=None, config_path=config_directory, config_name="config")
+@hydra.main(version_base=None, config_path="../../config", config_name="config")
 def retraining(config: DictConfig):
     OmegaConf.set_readonly(config, False)
 
